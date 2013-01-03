ddc311db375c4b8f9a6b802033d730673b90e3b9
Benjamin Alijagic <madebybenjamin@gmail.com>
2013-01-03 16:00:09 +0100

    Font changes

--- references.php
+++ references.php
@@ -11,6 +11,7 @@
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<script src="js/site.js" type="text/javascript" charset="utf-8"></script>
 	<script src="js/reveal/jquery.reveal.js" type="text/javascript" charset="utf-8"></script>
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
 </head>
 <body class="references">
 	
