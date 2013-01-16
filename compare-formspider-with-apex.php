aa00e1101c0aba21c809c1425241cf43cf10a3eb
oskan.savli <oskan.savli@gerger.co>
2013-01-16 21:13:13 +0200

    "Formspider vs Apex" page.

--- compare-formspider-with-apex.php
+++ compare-formspider-with-apex.php
@@ -27,58 +27,110 @@
 				<h2>Better tools, Better applications</h2>
 				<p>Formspider enables you to build web 2.0 applications you can only dream in Oracle Apex</p>
 			</div>
+			<div id="cta">Try now &rarr; &nbsp;&nbsp;
+				<a href="download.php" class="button blue mid-height" id="cloud"><span></span> Cloud </a>
+				<a href="download.php" class="button green mid-height" id="desktop"><span></span> Download </a>
+			</div>
 			
 			<div class="clearfix">
 			
-			<ul id="left">
-				<li class="head">Oracle Apex</li>
-				<li>PL/SQL and JavaScript. A lot of JavaScript if you want to build Web 2.0 applications.</li>
-				<li>Old fashioned Web 1.0 pages. Requires a lot of JavaScript and plumbing to build full Web 2.0 application.</li>
-				<li>No transaction layer.</li>
-				<li class="masterdetail-row">Virtually impossible to build a multi level master detail screen.</li>
-				<li>With your first line of HTML or JavaScript in Apex, cross browser support becomes your responsibility.</li>
-				<li>Web pages built by Apex are extremely dependent on HTML and JavaScript. Upgrading legacy technology is your responsibility.</li>
-				<li class="bestpractices-row">You have to learn, implement, test and maintain the best practices for security and performance in each application you build over and over again.</li>
-				<li class="paradigm-row">90’s style web page paradigm. Developers work with low level HTML tags such as < div > all the time.</li>
-				<li class="refactoring-row">Very difficult.</li>
-				<li>Very strange, thousand screen development tool. Unlike any other modern IDE.</li>
-			</ul>
-			
-			<ul id="center">
-				<li class="head"></li>
-				<li>Programming</li>
-				<li>Output</li>
-				<li>Transaction Management</li>
-				<li class="masterdetail-row">Master-Detail Screens</li>
-				<li>Cross Browser Support</li>
-				<li>Technology</li>
-				<li class="bestpractices-row">Best Practices</li>
-				<li class="paradigm-row">Paradigm</li>
-				<li class="refactoring-row">Refactoring</li>
-				<li>IDE</li>
-			</ul>
-			
-			<ul id="right">
-				<li class="head">Formspider</li>
-				<li>Formspider’s only programmaing language is PL/SQL. No Java, JavasScript or any other programming language is needed.</li>
-				<li>Modern Web 2.0 applications that run 100% AJAX out of the box.</li>
-				<li>Formspider gives you complete transaction control. Commit your changes or revert them to any state whenever you want.</li>
-				<li class="masterdetail-row">Formspider makes it very easy to implement three or more level master detail screens.</li>
-				<li>Formspider applications run in every major browser. Cross browser compatibility is Formspider’s responsibility, not yours.</li>
-				<li>Formspider applications are not client side technology dependant. Formspider applications start using new UI technologies by just upgrading the framework.</li>
-				<li class="bestpractices-row">Formspider JavaScript and middle tier libraries implement the best security and performance practices. Every Formspider application enjoys the benefits of these libraries automatically.</li>
-				<li class="paradigm-row">Developers work in the application development paradigm. They don’t worry about HTML tags. The word “page” does not even exist in Formspider vocabulary.</li>
-				<li class="refactoring-row">Formspider has reporting capabilities on UI object dependencies which makes refactoring a breeze. It even helps you refactoring.</li>
-				<li>Formspider has a first class modern IDE.</li>
-			</ul>
+<table id="comparison-table" style="table-layout:fixed; border-collapse:separate;" cellspacing="0" cellpadding="0" border="0">
+
+<tr class="head-row">
+	<td>  </td>
+	<td> Formspider </td>
+	<td> Oracle Apex </td>
+</tr>
+
+<tr>
+	<td style="width:120px;"> Programming </td>
+	<td style="width:420px;"> 
+		Formspider’s <a target="_blank" href="http://theformspider.com/blog/2011/09/19/benefits-of-formspider-single-programming-language/">only programmaing language is PL/SQL</a>. 
+		No Java, JavasScript or any other programming language is needed. 
+	</td>
+	<td style="width:420px;"> PL/SQL and JavaScript. A lot of JavaScript if you want to build Web 2.0 applications. </td>
+</tr>
+
+<tr>
+	<td> Output </td>
+	<td> <a target="_blank" href="http://theformspider.com/demos.php">Modern Web 2.0 applications</a> that run 100% AJAX out of the box. </td>
+	<td> Old fashioned Web 1.0 pages. Requires a lot of JavaScript and plumbing to build full Web 2.0 application. </td>
+</tr>
+
+<tr>
+	<td> Transaction Management </td>
+	<td> Formspider gives you complete transaction control. Commit your changes or revert them to any state whenever you want. </td>
+	<td> No transaction layer. </td>
+</tr>
+
+<tr>
+	<td> Master-Detail Screens </td>
+	<td> 
+		Formspider makes it very easy to implement
+		<a target="_blank" href="http://theformspider.com/blog/2012/06/04/a-three-levels-deep-story/">three or more level master detail screens</a>
+	</td>
+	<td> Virtually impossible to build a multi level master detail screen. </td>
+</tr>
+
+<tr>
+	<td> Cross Browser Support </td>
+	<td> 
+		Formspider applications
+		<a target="_blank" href="http://theformspider.com/blog/2011/03/22/true-cross-browser-web-applications/">run in every major browser</a>
+		. Cross browser compatibility is Formspider’s responsibility, not yours.
+	</td>
+	<td> With your first line of HTML or JavaScript in Apex, cross browser support becomes your responsibility. </td>
+</tr>
+
+<tr>
+	<td> Technology </td>
+	<td> 
+		Formspider applications are
+		<a target="_blank" href="http://theformspider.com/blog/2011/09/20/benefits-of-formspider-single-development-environment-for-all-platforms/">not client side technology dependant</a>
+		. Formspider applications start using new UI technologies by just upgrading the framework.
+	</td>
+	<td> Web pages built by Apex are extremely dependent on HTML and JavaScript. Upgrading legacy technology is your responsibility. </td>
+</tr>
+
+<tr>
+	<td> Best Practices </td>
+	<td> 
+		Formspider JavaScript and middle tier libraries implement the best security and
+		<a target="_blank" href="http://theformspider.com/blog/2011/09/22/benefits-of-formspider-great-performance-without-great-effort/">performance</a>
+		practices. Every Formspider application enjoys the benefits of these libraries automatically.
+	</td>
+	<td> You have to learn, implement, test and maintain the best practices for security and performance in each application you build over and over again. </td>
+</tr>
+
+<tr>
+	<td> Paradigm </td>
+	<td> Developers work in the application development paradigm. They don’t worry about HTML tags. The word “page” does not even exist in Formspider vocabulary. </td>
+	<td> 90’s style web page paradigm. Developers work with low level HTML tags such as < div > all the time. </td>
+</tr>
+
+<tr>
+	<td> Refactoring </td>
+	<td> Formspider has reporting capabilities on UI object dependencies which makes refactoring a breeze. It even helps you refactoring. </td>
+	<td> Very difficult. </td>
+</tr>
+
+<tr>
+	<td> IDE </td>
+	<td> Formspider has a
+		<a target="_blank" href="http://theformspider.com/blog/2011/09/21/benefits-of-formspider-productive-development-environment/">first class modern IDE</a> 
+	</td>
+	<td> Very strange, thousand screen development tool. Unlike any other modern IDE. </td>
+</tr>
+
+</table>
 			
 			</div>
 			
 		
-			<div id="cta">
+			<!--div id="cta">
 				<div><h2>Try now for free:</h2></div>
 					<a href="#" id="cloud" class="button blue"><span></span>  Cloud</a> <a href="download.php" id="desktop" class="button blue"><span></span>  Desktop</a></a>
-			</div>
+			</div-->
 
 		</div><!-- /content -->
 
