783660f9e4c4e93a8306c509d7717d8a87fc5dcf
oskan.savli <oskan.savli@gerger.co>
2013-01-16 21:03:39 +0200

    Relative url's on header.

--- assets/header.php
+++ assets/header.php
@@ -2,14 +2,14 @@
 	<div id="header">
 		<a id="logo" href="index.php"><h1>Formspider</h1></a>
 		<ul id="nav">
-			<li<? if($page == 'index'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/index.php">Home</a></li>
-			<li<? if($page == 'product'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/product.php">Product</a></li>	
-			<li<? if($page == 'demos'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/demos.php">Demos</a></li>	
-			<li<? if($page == 'learn'): ?> class="active" <? endif ?>><a href="http://theformspider.com/test/learningcenter">Learn</a></li>
+			<li<? if($page == 'index'): ?> class="active"<? endif ?>><a href="index.php">Home</a></li>
+			<li<? if($page == 'product'): ?> class="active"<? endif ?>><a href="product.php">Product</a></li>	
+			<li<? if($page == 'demos'): ?> class="active"<? endif ?>><a href="demos.php">Demos</a></li>	
+			<li<? if($page == 'learn'): ?> class="active" <? endif ?>><a href="learningcenter">Learn</a></li>
 			<li<? if($page == 'qa'): ?> class="active" <? endif ?>><a href="http://osqa.theformspider.com" target="_blank">Q&A</a></li>
-			<li<? if($page == 'pricing'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/pricing.php">Pricing</a></li>
-			<li<? if($page == 'references'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/references.php">References</a></li>
-			<li<? if($page == 'blog'): ?> class="active" <? endif ?>><a href="http://theformspider.com/test/blog">Blog</a></li>
-			<li<? if($page == 'about-us'): ?> class="active"<? endif ?>><a href="http://theformspider.com/test/about-us.php">About us</a></li>
+			<li<? if($page == 'pricing'): ?> class="active"<? endif ?>><a href="pricing.php">Pricing</a></li>
+			<li<? if($page == 'references'): ?> class="active"<? endif ?>><a href="references.php">References</a></li>
+			<li<? if($page == 'blog'): ?> class="active" <? endif ?>><a href="blog">Blog</a></li>
+			<li<? if($page == 'about-us'): ?> class="active"<? endif ?>><a href="about-us.php">About us</a></li>
 		<ul>
 	</div><!-- /header -->
