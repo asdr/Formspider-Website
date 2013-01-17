a496240d23c61a208636bb0043588dc23889f933
oskan.savli <oskan.savli@gerger.co>
2013-01-17 15:04:12 +0200

    Demo Page: Changed wrong images, added missing demos.

--- demos.php
+++ demos.php
@@ -24,97 +24,258 @@
 	<div id="content">
 		
 		<h2>Demos and Examples</h2>
-		<div id="cta-right">Try now for free &rarr; <a href="download.php" id="cloud" class="button silver small">Cloud</a><a href="download.php" id="desktop" class="button silver small">Desktop</a></div>
+		<div id="cta-right">Try now for free &rarr; <a href="download.php" id="cloud" class="button silver small">Cloud</a><a href="download.php" id="desktop" class="button silver small"> Download </a></div>
 			
 		<ul>
+		
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoGrid">
 				<img src="images/demos/demo_grid.jpg" width="202" height="90" alt="1">
 				<h4>Grid</h4>
 				<p>A databound grid component with paging example.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoRowAndCellLevelGridAttributes">
 				<img src="images/demos/demo_grid.jpg" width="202" height="90" alt="1">
 				<h4>Advanced Grid</h4>
 				<p>Row & cell level attributes of the grid component.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoMasterDetail">
 				<img src="images/demos/demo_masterdetailform.jpg" width="202" height="90" alt="1">
 				<h4>Master Detail Form</h4>
 				<p>A classical form type, master-detail.</p>
 			</a></li>
+			
 			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoMasterDetailGrids">
 				<img src="images/demos/demo_masterdetailgrid.jpg" width="202" height="90" alt="1">
 				<h4>Master Detail Grids</h4>
 				<p>A master-detail form build with two grids.</p>
 			</a></li>
+			
+			
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoThreeLevelMasterDetailGrids">
-				<img src="images/demo/1.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/demoThreeLevelMasterDetailGrids.PNG" width="130" height="90" alt="1">
 				<h4>3-Level Master Detail Grids</h4>
 				<p>A master-detail form build with three grids.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoTree">
-				<img src="images/demos/demo_tree.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/tree.png" width="130" height="90" alt="1">
 				<h4>Tree</h4>
 				<p>An example of a tree loaded by multiple datasources.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoCalendar">
-				<img src="images/demos/demo_calendar.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/demoCalendar.PNG" width="130" height="90" alt="1">
 				<h4>Calendar</h4>
 				<p>Formspider calendar sample.</p>
 			</a></li>
+			
 			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoPanel">
-				<img src="images/demos/demo_panels.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/panels.png" width="130" height="90" alt="1">
 				<h4>Panels</h4>
 				<p>Formspider panel types. Simple, tab, accordion, split panel samples.</p>
 			</a></li>
+			
+			
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoExcel">
 				<img src="images/demos/demo_excel.jpg" width="202" height="90" alt="1">
 				<h4>Exporting Data to Excel</h4>
 				<p>Exporting data to Excel through Formspider grids and datasources.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoMap">
 				<img src="images/demos/demo_map.jpg" width="202" height="90" alt="1">
 				<h4>Map</h4>
 				<p>Formspider map component sample.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoPagingComponents">
-				<img src="images/demo/1.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/demoPagingComponents.PNG" width="130" height="90" alt="1">
 				<h4>Paging Components</h4>
 				<p>Formspider paging components sample.</p>
 			</a></li>
+			
 			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoExternalMethod">
 				<img src="images/demos/demo_externalmethod.jpg" width="202" height="90" alt="1">
 				<h4>External Method</h4>
 				<p>Implementing "Remember Me" functionality through Formspider External Method.</p>
 			</a></li>
+			
+			
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoJasperReports">
 				<img src="images/demos/demo_jasperreports.jpg" width="202" height="90" alt="1">
 				<h4>Jasper Reports</h4>
 				<p>Integrating JasperReports into Formspider.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoOracleReports">
 				<img src="images/demos/demo_oraclereports.jpg" width="202" height="90" alt="1">
 				<h4>Oracle Reports</h4>
 				<p>Integrating OracleReports into Formspider.</p>
 			</a></li>
+			
 			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoTableLayout">
-				<img src="images/demo/1.jpg" width="202" height="90" alt="1">
+				<img src="./images/demo/tableLayout.png" width="130" height="90" alt="1">
 				<h4>Table Layout</h4>
 				<p>An example of a panel managed by Table Layout.</p>
 			</a></li>
+			
 			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoTableLayoutForm">
-				<img src="http://placehold.it/202x90" width="202" height="90" alt="1">
+				<img src="./images/demo/demoTableLayoutForm.png" width="130" height="90" alt="1">
 				<h4>Table Layout Form</h4>
 				<p>A form with table layout and validation sample.</p>
 			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoXYLayout">
+				<img src="./images/demo/xyLayout.png" width="130" height="90" alt="1">
+				<h4>XY Layout</h4>
+				<p>An example of a panel managed by XY Layout.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoBorderLayout">
+				<img src="./images/demo/borderLayout.png" width="130" height="90" alt="1">
+				<h4>Border Layout</h4>
+				<p>An example of a panel managed by Border Layout.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoMovingGridRow">
+				<img src="./images/demo/demoMovingGridRow.png" width="130" height="90" alt="1">
+				<h4>Moving a Grid Row</h4>
+				<p>Changing the order of a row in grid.</p>
+			</a></li>
+			
+			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoSnapshotMechanism">
+				<img src="./images/demo/demoSnapshotMechanism.PNG" width="130" height="90" alt="1">
+				<h4>Datasource Snapshot Mechanism</h4>
+				<p>A form bound to a database table via Formspider Datasource object.</p>
+			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoDataBinding">
+				<img src="./images/demo/dataBinding.png" width="130" height="90" alt="1">
+				<h4>Data Binding</h4>
+				<p>Binding components to a database query using a Datasource.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoBindingForm">
+				<img src="./images/demo/bindingForm.png" width="130" height="90" alt="1">
+				<h4>Binding Form</h4>
+				<p>A form bound to a database table via Formspider Datasource object.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoMenuBar">
+				<img src="./images/demo/menubar.png" width="130" height="90" alt="1">
+				<h4>Menu Bar</h4>
+				<p>Formspider menu bar sample.</p>
+			</a></li>
+			
+			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#Charts">
+				<img src="./images/demo/charts.png" width="130" height="90" alt="1">
+				<h4>Charts</h4>
+				<p>Chart components in formspider.</p>
+			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoLOV">
+				<img src="./images/demo/demoLOV.PNG" width="130" height="90" alt="1">
+				<h4>LOV</h4>
+				<p>Formspider LOV sample.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#Survey">
+				<img src="./images/demo/buttongroup.png" width="130" height="90" alt="1">
+				<h4>Button Group and Radio Buttons</h4>
+				<p>How to use button group and radio buttons.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoDomains">
+				<img src="./images/demo/demoDomains.PNG" width="130" height="90" alt="1">
+				<h4>Domains</h4>
+				<p>Formspider domain usage sample.</p>
+			</a></li>
+			
+			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoFileUpload">
+				<img src="./images/demo/demoFileUpload.PNG" width="130" height="90" alt="1">
+				<h4>File Upload</h4>
+				<p>Formspider file upload component.</p>
+			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoInternationalization">
+				<img src="./images/demo/demoInternationalization.PNG" width="130" height="90" alt="1">
+				<h4>Internationalization</h4>
+				<p>Internationalization in Formspider.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoEvent">
+				<img src="./images/demo/event.png" width="130" height="90" alt="1">
+				<h4>Events & Actions</h4>
+				<p>Formspider event and action sample.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoAlert">
+				<img src="./images/demo/demoAlert.PNG" width="130" height="90" alt="1">
+				<h4>Alerts</h4>
+				<p>Formspider alert samples.</p>
+			</a></li>
+			
+			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoKeyEvents">
+				<img src="./images/demo/demoFileUpload.PNG" width="130" height="90" alt="1">
+				<h4>Key Events</h4>
+				<p>Using Formspider key events.</p>
+			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoTimer">
+				<img src="./images/demo/demoTimer.PNG" width="130" height="90" alt="1">
+				<h4>Timer</h4>
+				<p>Working with a timer in Formspider.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.jsp?name=DemoMain#DemoShuttle">
+				<img src="./images/demo/demoShuttle.PNG" width="130" height="90" alt="1">
+				<h4>Shuttle</h4>
+				<p>How to build a shuttle component with Formspider.</p>
+			</a></li>
+			
+			<li><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoDialog">
+				<img src="./images/demo/demoDialog.PNG" width="130" height="90" alt="1">
+				<h4>Dialogs</h4>
+				<p>Modal and Non-Modal dialog samples.</p>
+			</a></li>
+			
+			<li class="last-in-row"><a href="http://formspideronline.com/formspider/main.html?name=DemoMain#DemoDependentComboBoxes">
+				<img src="./images/demo/demoDependentComboBoxes.png" width="130" height="90" alt="1">
+				<h4>Dependent Combo Boxes</h4>
+				<p>Combo box populated dynamically when changing another one.</p>
+			</a></li>
+			
+			
+			
+			<li><a href="http://formspideronline.com/formspider/main.jsp?name=DemoMain#DemoTemplateApplication">
+				<img src="./images/demo/demoTemplateApplication.PNG" width="130" height="90" alt="1">
+				<h4>Tree driven Tabbed Application</h4>
+				<p>Building a tree driven tabbed application with Formspider.</p>
+			</a></li>
+			
 		</ul>
 		
 		<div id="cta">
 			<a class="button silver" href="compare-formspider-with-apex.php">Compare Formspider with Apex</a> 
 			<div>&mdash; or &mdash;</div>
 			
-			<div><h2>Try now for free:</h2></div>
-				<a href="#" id="cloud" class="button blue"><span></span>  Cloud</a> <a href="download.php" id="desktop" class="button blue"><span></span>  Desktop</a></a>
+			<div><h2> Try now </h2></div>
+				<a href="#" id="cloud" class="button blue"><span></span> Cloud </a> <a href="download.php" id="desktop" class="button green"><span></span> Download </a></a>
 		</div>	
 		
 	</div><!-- /content -->
