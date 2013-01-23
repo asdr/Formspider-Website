4957c104b276ba32f9df340f0449df9e1f8a214b
ibrahim.sandalli <ibrahim.sandalli@gerger.co>
2013-01-23 15:01:17 +0200

    changed "<?" to "<?php"

--- assets/header.php
+++ assets/header.php
@@ -2,14 +2,14 @@
 	<div id="header">
 		<a id="logo" href="index.php"><h1>Formspider</h1></a>
 		<ul id="nav">
-			<li<? if($page == 'index'): ?> class="active"<? endif ?>><a href="index.php">HOME</a></li>
-			<li<? if($page == 'product'): ?> class="active"<? endif ?>><a href="product.php">PRODUCT</a></li>	
-			<li<? if($page == 'demos'): ?> class="active"<? endif ?>><a href="demos.php">DEMOS</a></li>	
-			<li<? if($page == 'learn'): ?> class="active" <? endif ?>><a href="learningcenter">LEARN</a></li>
-			<li<? if($page == 'qa'): ?> class="active" <? endif ?>><a href="http://osqa.theformspider.com" target="_blank">Q&A</a></li>
-			<li<? if($page == 'pricing'): ?> class="active"<? endif ?>><a href="pricing.php">PRICING</a></li>
-			<li<? if($page == 'references'): ?> class="active"<? endif ?>><a href="references.php">REFERENCES</a></li>
-			<li<? if($page == 'blog'): ?> class="active" <? endif ?>><a href="blog/index.php">BLOG</a></li>
-			<li<? if($page == 'about-us'): ?> class="active"<? endif ?>><a href="aboutus.php">ABOUT US</a></li>
+			<li<?php if($page == 'index'): ?> class="active"<?php endif ?>><a href="index.php">HOME</a></li>
+			<li<?php if($page == 'product'): ?> class="active"<?php endif ?>><a href="product.php">PRODUCT</a></li>	
+			<li<?php if($page == 'demos'): ?> class="active"<?php endif ?>><a href="demos.php">DEMOS</a></li>	
+			<li<?php if($page == 'learn'): ?> class="active" <?php endif ?>><a href="learningcenter">LEARN</a></li>
+			<li<?php if($page == 'qa'): ?> class="active" <?php endif ?>><a href="http://osqa.theformspider.com" target="_blank">Q&A</a></li>
+			<li<?php if($page == 'pricing'): ?> class="active"<?php endif ?>><a href="pricing.php">PRICING</a></li>
+			<li<?php if($page == 'references'): ?> class="active"<?php endif ?>><a href="references.php">REFERENCES</a></li>
+			<li<?php if($page == 'blog'): ?> class="active" <?php endif ?>><a href="blog/index.php">BLOG</a></li>
+			<li<?php if($page == 'about-us'): ?> class="active"<?php endif ?>><a href="aboutus.php">ABOUT US</a></li>
 		<ul>
 	</div><!-- /header -->
