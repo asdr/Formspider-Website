c17d26d862b5afffc92e1c3cf96c9f3cbf0d1ce3
oskan.savli <oskan.savli@gerger.co>
2013-02-11 17:09:11 +0200

    Added Download tab.

--- assets/header.php
+++ assets/header.php
@@ -4,7 +4,8 @@
 		<ul id="nav">
 			<li<?php if($page == 'index'): ?> class="active"<?php endif ?>><a href="default.php">HOME</a></li>
 			<li<?php if($page == 'product'): ?> class="active"<?php endif ?>><a href="product.php">PRODUCT</a></li>	
-			<li<?php if($page == 'demos'): ?> class="active"<?php endif ?>><a href="demos.php">DEMOS</a></li>	
+			<li<?php if($page == 'demos'): ?> class="active"<?php endif ?>><a href="demos.php">DEMOS</a></li>
+			<li<?php if($page == 'download'): ?> class="active"<?php endif ?>><a href="download.php">DOWNLOAD</a></li>
 			<li<?php if($page == 'learn'): ?> class="active" <?php endif ?>><a href="learningcenter">LEARN</a></li>
 			<li<?php if($page == 'qa'): ?> class="active" <?php endif ?>><a href="http://osqa.theformspider.com" target="_blank">Q&A</a></li>
 			<li<?php if($page == 'pricing'): ?> class="active"<?php endif ?>><a href="pricing.php">PRICING</a></li>
