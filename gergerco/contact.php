42ea17b52704f50996d531fed0e632c3595692c2
Serdar Gökçen <gokcen.serdar@gmail.com>
2014-09-22 15:02:59 +0300

    Google Analytics code updated with new version

--- gergerco/contact.php
+++ gergerco/contact.php
@@ -1,117 +1,235 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width = 980" />
-<title>Gerger</title>
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<link href="css/product.css" rel="stylesheet" type="text/css" />
-<script src="css/css_browser_selector.js" type="text/javascript"></script>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-1251493-11']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-</head>
-
-<body>
-
-<div class="container">
-  <div class="header">
-  	<?php $link_name= "Contact" ?>
-  	<?php include ('mainmenu.php'); ?>
-
-  <!-- end .header -->
-  </div>
-  
-  <div class="content"> 
-    <br /> 	
-    <br /> 	
-  	<table>
-  	  <tr>
-  	    <td>
-  	      <div  class="box" style="height:280px; padding-left:60px;">
-  	      <table>
-	        <tr>
-			  <td colspan="2">
-			    <br/>
-			    <h1 align="left" style="font-size: 15px; font-style: italic; font-weight: bold;"><b>Gerger, Istanbul TR</b></h1>
-			    <br/>
-			  	<p>Address : ITU ARI-1 Teknokent No:15 Maslak Sisli - Istanbul / Turkey</p>
-			  	<p>Phone : +90 212 328 27 75</p>
-			  	<p>Email : info(at)gerger.co</p>
-			  	<br />
-			  	<div style="padding-left:11px;">
-			    <!--span style="width:170px; float:left"-->
-		            <a title="Send us an e-mail" href="mailto:getinfo@gergerconsulting.com"><img align="middle" src="images/at_icon.png"/></a>
-		            <a target="_blank" title="Follow @gerger on Twitter" href="http://twitter.com/yalimgerger"><img align="middle" src="images/Twitter.png"/></a>
-		  	        <a target="_blank" title="Become a fan of Gerger" href="http://www.facebook.com/group.php?gid=127533637265865"><img align="middle" src="images/Facebook.png"/></a>
-		            <a target="_blank" title="Join the Gerger Network" href="http://www.linkedin.com/company/378628"><img align="middle" src="images/Linkedin.png"/></a>
-		            <a target="_blank" title="Subscribe to RSS Feed" href="http://www.gerger.co/blog/?feed=rss2"><img  align="middle" src="images/rss2.png"  alt="Subscribe to RSS Feed"/></a>
-	            <!--/span-->
-	  	       </div>
-			  </td>  		
-	  	   </tr>
-          </table>
-          </div>
-  	    </td>
-  	    <td>
-  	      <div align="left">
-  	        <iframe width="350" height="280" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/?ie=UTF8&amp;ll=41.105941,29.016175&amp;spn=0.003496,0.006427&amp;t=h&amp;z=18&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/?ie=UTF8&amp;ll=41.105941,29.016175&amp;spn=0.003496,0.006427&amp;t=h&amp;z=18&amp;source=embed"></iframe>			
-	  	    <!--iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=41.108189,29.033614&amp;num=1&amp;t=k&amp;sll=41.107926,29.03421&amp;sspn=0.003327,0.009012&amp;ie=UTF8&amp;ll=41.107926,29.03421&amp;spn=0.001663,0.004506&amp;z=14&amp;output=embed"></iframe>
-	  	    <!--iframe width="350" height="280" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?source=s_d&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16&amp;output=embed"></iframe><br /><small>Şunu daha büyük bir haritada görüntüle: <a href="http://maps.google.com/maps/ms?source=embed&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16" style="color:#0000FF;text-align:left">Başlıksız</a></small-->
-	  	  </div>
-  	    </td>
-  	  </tr>
-  	</table>
-
-    
-  	<!--div align="left" style="top:10px; padding-left:14px;">
-  	   <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?source=s_d&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16&amp;output=embed"></iframe><br /><small>Şunu daha büyük bir haritada görüntüle: <a href="http://maps.google.com/maps/ms?source=embed&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16" style="color:#0000FF;text-align:left">Başlıksız</a></small>
-  	</div-->
-  	  	<?php include ('footer.php'); ?>
-  </div>
-  </div>
-  
- 
-   <!--div class="content">  	
-  	<br/>
-  	<br/>
-  	<p><a style="font-size:20px; font-weight: bold;">Gerger, Istanbul TR</a></p>
-  	<p><a style="font-size:15px; font-weight: bold;">Address : ITU ARI2 Teknokent A5.6 Maslak Sisli - Istanbul / Turkey</a></p>
-  	<p><a style="font-size:15px; font-weight: bold;">Phone : +90 212 328 27 75</a></p>
-  	<p><a style="font-size:15px; font-weight: bold;">Email : getinfo(at)gergerconsulting.com</a></p>
-  	<br />
-  	<br />
-  	
-  	<div style="top:10px; padding-left:11px;">
-		
-  	  <span style="width:170px; float:left">
-            <a title="Send us an e-mail" href="mailto:getinfo@gergerconsulting.com"><img align="middle" src="images/at_icon.png"/></a>
-  	        <a target="_blank" title="Become a fan of Gerger" href="http://www.facebook.com/group.php?gid=127533637265865"><img align="middle" src="images/Facebook.png"/></a>
-            <a target="_blank" title="Join the Gerger Network" href="http://www.linkedin.com/company/378628"><img align="middle" src="images/Linkedin.png"/></a>
-            <a target="_blank" title="Receive Gerger Blog Updates" href="#"><img align="middle" src="images/rss2.png"/></a>
-            </span>
-  	</div>
-  	
-  	<br />
-  	<br />
-  	<br /-->
-  	
-  	
-  
-  
-  </div>
-</body>
-</html>
-
-
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<meta name="viewport" content="width = 980" />
+
+<title>Gerger</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<link href="css/product.css" rel="stylesheet" type="text/css" />
+
+<script src="css/css_browser_selector.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+
+
+  var _gaq = _gaq || [];
+
+  _gaq.push(['_setAccount', 'UA-1251493-11']);
+
+  _gaq.push(['_trackPageview']);
+
+
+
+  (function() {
+
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+
+    // ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+  })();
+
+
+
+</script>
+
+</head>
+
+
+
+<body>
+
+
+
+<div class="container">
+
+  <div class="header">
+
+  	<?php $link_name= "Contact" ?>
+
+  	<?php include ('mainmenu.php'); ?>
+
+
+
+  <!-- end .header -->
+
+  </div>
+
+  
+
+  <div class="content"> 
+
+    <br /> 	
+
+    <br /> 	
+
+  	<table>
+
+  	  <tr>
+
+  	    <td>
+
+  	      <div  class="box" style="height:280px; padding-left:60px;">
+
+  	      <table>
+
+	        <tr>
+
+			  <td colspan="2">
+
+			    <br/>
+
+			    <h1 align="left" style="font-size: 15px; font-style: italic; font-weight: bold;"><b>Gerger, Istanbul TR</b></h1>
+
+			    <br/>
+
+			  	<p>Address : ITU ARI-1 Teknokent No:15 Maslak Sisli - Istanbul / Turkey</p>
+
+			  	<p>Phone : +90 212 328 27 75</p>
+
+			  	<p>Email : info(at)gerger.co</p>
+
+			  	<br />
+
+			  	<div style="padding-left:11px;">
+
+			    <!--span style="width:170px; float:left"-->
+
+		            <a title="Send us an e-mail" href="mailto:getinfo@gergerconsulting.com"><img align="middle" src="images/at_icon.png"/></a>
+
+		            <a target="_blank" title="Follow @gerger on Twitter" href="http://twitter.com/yalimgerger"><img align="middle" src="images/Twitter.png"/></a>
+
+		  	        <a target="_blank" title="Become a fan of Gerger" href="http://www.facebook.com/group.php?gid=127533637265865"><img align="middle" src="images/Facebook.png"/></a>
+
+		            <a target="_blank" title="Join the Gerger Network" href="http://www.linkedin.com/company/378628"><img align="middle" src="images/Linkedin.png"/></a>
+
+		            <a target="_blank" title="Subscribe to RSS Feed" href="http://www.gerger.co/blog/?feed=rss2"><img  align="middle" src="images/rss2.png"  alt="Subscribe to RSS Feed"/></a>
+
+	            <!--/span-->
+
+	  	       </div>
+
+			  </td>  		
+
+	  	   </tr>
+
+          </table>
+
+          </div>
+
+  	    </td>
+
+  	    <td>
+
+  	      <div align="left">
+
+  	        <iframe width="350" height="280" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/?ie=UTF8&amp;ll=41.105941,29.016175&amp;spn=0.003496,0.006427&amp;t=h&amp;z=18&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/?ie=UTF8&amp;ll=41.105941,29.016175&amp;spn=0.003496,0.006427&amp;t=h&amp;z=18&amp;source=embed"></iframe>
+			
+	  	    <!--iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?q=41.108189,29.033614&amp;num=1&amp;t=k&amp;sll=41.107926,29.03421&amp;sspn=0.003327,0.009012&amp;ie=UTF8&amp;ll=41.107926,29.03421&amp;spn=0.001663,0.004506&amp;z=14&amp;output=embed"></iframe>
+
+	  	    <!--iframe width="350" height="280" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?source=s_d&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16&amp;output=embed"></iframe><br /><small>Şunu daha büyük bir haritada görüntüle: <a href="http://maps.google.com/maps/ms?source=embed&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16" style="color:#0000FF;text-align:left">Başlıksız</a></small-->
+
+	  	  </div>
+
+  	    </td>
+
+  	  </tr>
+
+  	</table>
+
+
+
+    
+
+  	<!--div align="left" style="top:10px; padding-left:14px;">
+
+  	   <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps/ms?source=s_d&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16&amp;output=embed"></iframe><br /><small>Şunu daha büyük bir haritada görüntüle: <a href="http://maps.google.com/maps/ms?source=embed&amp;saddr=&amp;daddr=41.108023,29.035728&amp;geocode=&amp;hl=tr&amp;mra=dme&amp;mrcr=0&amp;mrsp=1&amp;sz=17&amp;ie=UTF8&amp;msa=0&amp;msid=218304334541643483322.00049bb1f57b9a5d2837f&amp;ll=41.108338,29.033968&amp;spn=0.00561,0.00912&amp;z=16" style="color:#0000FF;text-align:left">Başlıksız</a></small>
+
+  	</div-->
+
+  	  	<?php include ('footer.php'); ?>
+
+  </div>
+
+  </div>
+
+  
+
+ 
+
+   <!--div class="content">  	
+
+  	<br/>
+
+  	<br/>
+
+  	<p><a style="font-size:20px; font-weight: bold;">Gerger, Istanbul TR</a></p>
+
+  	<p><a style="font-size:15px; font-weight: bold;">Address : ITU ARI2 Teknokent A5.6 Maslak Sisli - Istanbul / Turkey</a></p>
+
+  	<p><a style="font-size:15px; font-weight: bold;">Phone : +90 212 328 27 75</a></p>
+
+  	<p><a style="font-size:15px; font-weight: bold;">Email : getinfo(at)gergerconsulting.com</a></p>
+
+  	<br />
+
+  	<br />
+
+  	
+
+  	<div style="top:10px; padding-left:11px;">
+
+		
+
+  	  <span style="width:170px; float:left">
+
+            <a title="Send us an e-mail" href="mailto:getinfo@gergerconsulting.com"><img align="middle" src="images/at_icon.png"/></a>
+
+  	        <a target="_blank" title="Become a fan of Gerger" href="http://www.facebook.com/group.php?gid=127533637265865"><img align="middle" src="images/Facebook.png"/></a>
+
+            <a target="_blank" title="Join the Gerger Network" href="http://www.linkedin.com/company/378628"><img align="middle" src="images/Linkedin.png"/></a>
+
+            <a target="_blank" title="Receive Gerger Blog Updates" href="#"><img align="middle" src="images/rss2.png"/></a>
+
+            </span>
+
+  	</div>
+
+  	
+
+  	<br />
+
+  	<br />
+
+  	<br /-->
+
+  	
+
+  	
+
+  
+
+  
+
+  </div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
