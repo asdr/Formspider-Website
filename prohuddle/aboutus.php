14f913536b98a11667a5adc02e5f3d33eb515714
Serdar Gökçen <serdar.gokcen@gerger.co>
2015-12-29 12:06:23 +0200

    ProHuddle - ProHuddle initiated.

---
+++ prohuddle/aboutus.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>ProHuddle - Engaging Webinars with IT Professinals</title>
+
+        <link rel="stylesheet" type="text/css" href="css/prohuddle.css?v=0001" />
+    </head>
+    <body>
+        <div class="header">
+            <div class="headerContent">
+                <div class="logo">
+                    <a href="http://www.prohuddle.com">ProHuddle</a>
+                </div>
+                <div class="menubar">
+                    <ul>
+                        <li><a href="default.php">Home</a></li>
+                        <li><a href="webinars.php">Webinars</a></li>
+                        <li><a href="blog.php">Blog</a></li>
+                        <li><a href="aboutus.php">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ prohuddle/default.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    <title>ProHuddle - Engaging Webinars with IT Professinals</title>
+
+    <link rel="stylesheet" type="text/css" href="css/prohuddle.css?v=0001" />
+</head>
+<body class="default_page">
+    <div class="header">
+        <div class="headerContent">
+            <div class="logo">
+                <a href="http://www.prohuddle.com">ProHuddle</a>
+            </div>
+            <div class="menubar">
+                <ul>
+                    <li><a href="default.php">Home</a></li>
+                    <li><a href="webinars.php">Webinars</a></li>
+                    <li><a href="blog.php">Blog</a></li>
+                    <li><a href="aboutus.php">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="message">
+            <div class="main">Engaging Webinars with IT Professionals</div>
+            <div class="sub">ProHuddle helps you solve challenges in your daily work and improve your professional life.</div>
+        </div>
+        <div class="signmeup">
+              <form action="default.php?signmeup" class="signupform" method="post">
+                <div class="field">
+                  <input autofocus="autofocus" type="text" class="text-input" autocomplete="off" placeholder="Your name">
+                </div>
+                <div class="field">
+                  <input type="email" class="text-input email-input" autocomplete="off" placeholder="E-Mail">
+                </div>
+                <div class="field liketobeapresenter">
+                  <input type="checkbox" name="liketobeapresenter" value="0" /> <label for="liketobeapresenter">I am also interested in being a presenter.</label>
+                </div>
+                <div class="field">
+                    <input type="submit" class="signup-btn" value="Inform me about upcoming webinars!" />
+                </div>
+              </form>
+        </div>
+        <div class="wontspam">
+            We will never spam you. We will never share your email with a third party. You can unsubscribe at any time with one click.
+        </div>
+    </div>
+    <div class="assets">
+        <div class="asset">
+            <div class="title"><img src="images/homepage/free.png" />Free</div>
+            <p>Watch pundits in any field you are interested in, right from your PC or mobile device, anywhere, anytime and for free.</p>
+        </div>
+        <div class="asset">
+            <div class="title"><img src="images/homepage/arm_muscle.png" />Empowering</div>
+            <p>Learn about new technologies and products you can use in your professional lives from trusted sources. Improve your skills in deeply technical presentations. </p>
+        </div>
+        <div class="asset">
+            <div class="title"><img src="images/homepage/lightbulb.png" />Effective</div>
+            <p>Connect with like minded peers from all over the world. Collaborate, ask questions, get stuff done together during live sessions.</p>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="line"></div>
+
+        <div class="links">
+            <ul>
+                <li><a href="default.php">Home</a></li>
+                <li><a href="aboutus.php">About us</a></li>
+            </ul>
+            <ul>
+                <li><a href="webinars.php">Webinars</a></li>
+                <li><a href="http://blog.prohuddle.com">Blog</a></li>
+            </ul>           
+            <ul class="social">
+                <li class="social twitter"><a alt="Twitter" href="http://www.twitter.com/pro_huddle" target="_blank">Twitter</a></li>
+                <li class="social email last"><a alt="E-Mail Address" href="mailto:contact@prohuddle.com">Email</a></li>
+            </ul>
+        </div>
+        
+        <div class="line"></div>
+        
+        <div class="copyright">
+            <div class="inlineblock">&copy; Copyright Gerger 2016</div>
+            <div class="inlineblock">Designed and developed by <a href="http://www.gerger.co" target="_blank">Gerger</a> at Istanbul Technical University <a href="http://www.ariteknokent.com.tr" target="_blank">ARI Technopark</a>.</div>
+        </div>
+    </div>
+
+</body>
+</html>
